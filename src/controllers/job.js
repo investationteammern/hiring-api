a0a97bc8c134ea1d4002118983c12fe483728420
karthiksundararajan12 <karthik.sundararajan12@gmail.com>
2025-06-25 15:35:21 +0530

    added export data api

--- src/controllers/job.js
+++ src/controllers/job.js
@@ -1,11 +1,54 @@
 import JobCategory from "../models/jobCategory.js";
 
 export const createCategory = async (req, res) => {
-    const { name } = req.body;
+    try {
+        const { name } = req.body;
 
-    const category = new JobCategory({ name });
+        const category = new JobCategory({ name });
 
-    await category.save();
-    res.status(201).json({ success: true, message: 'Job category created' })
+        await category.save();
+        res.status(201).json({ success: true, message: 'Job category created' })
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'Server errror.' })
+    }
+
 }
 
+export const listCategories = async (req, res) => {
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 3
+    const skip = (page - 1) * limit
+    const search = req.query.search || '';
+    const fetchAll = req.query.all === 'true'
+
+    const query = {};
+    if (search) {
+        query.name = { $regex: search, $options: 'i' } // 'i' for case-insensitive
+    }
+    try {
+        const response = await JobCategory.find(query)
+            .skip(skip)
+            .limit(limit)
+        const total = await JobCategory.countDocuments()
+        if (response) {
+            return res.status(200).json({
+                currentPage: page,
+                totalPages: Math.ceil(total / limit),
+                totalItems: total,
+                response
+            })
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' })
+    }
+}
+
+export const exportData = async (req, res) => {
+    try {
+        const categories = await JobCategory.find();
+        res.status(200).json({ data: categories });
+    } catch (error) {
+        res.status(500).json({ message: 'Export failed' });
+
+    }
+}
--- src/routes/jobCategory.js
+++ src/routes/jobCategory.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createCategory } from '../controllers/job.js';
+import { createCategory, exportData, listCategories } from '../controllers/job.js';
 
 
 const router = express.Router();
 
 router.post('/job', createCategory);
-
+router.get('/categories', listCategories);
+router.get('/categories/export', exportData);
 export default router; 
