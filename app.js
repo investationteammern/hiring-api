eff8bc373269aeb745214bc017504cc4e66b4560
karthiksundararajan12 <karthik.sundararajan12@gmail.com>
2025-06-25 15:38:47 +0530

    Added cors origin

--- app.js
+++ app.js
@@ -8,8 +8,24 @@ import JobRoutes from './src/routes/jobCategory.js';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://hiring-web-blush.vercel.app/',
+]
 
+// ✅ Use CORS with dynamic origin checking
+app.use(cors({
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl)
+        if (!origin) return callback(null, true)
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true)
+        } else {
+            return callback(new Error('Not allowed by CORS'))
+        }
+    },
+    // credentials: true, // If using cookies or sessions
+}))
 
 
 app.use('/v1/api', JobRoutes);
